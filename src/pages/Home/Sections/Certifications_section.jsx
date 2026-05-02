a2a3d600b76a1b2b6fa17147d06d1c3223530e07
OmarKhaled-00 <ok8086143@gmail.com>
2026-05-02 12:14:06 +0300

    Fix Layout in certifications section

--- src/pages/Home/Sections/Certifications_section.jsx
+++ src/pages/Home/Sections/Certifications_section.jsx
@@ -27,7 +27,7 @@ function Certifications_section() {
             {Certifications_data.map((c) => (
               <div
                 key={c.id}
-                className={`${isDark ? "h-180 border border-solid border-white" : "h-200 shadow-[2px_2px_5px_#BABECC,-5px_-5px_10px_#ffffff73]"} flex flex-col justify-between overflow-hidden rounded-2xl *:capitalize`}
+                className={`${isDark ? "h-180 border border-solid border-white" : "h-200 shadow-[2px_2px_5px_#BABECC,-5px_-5px_10px_#ffffff73]"} flex flex-col justify-around overflow-hidden rounded-2xl *:capitalize`}
               >
                 <img
                   loading="lazy"
@@ -37,7 +37,7 @@ function Certifications_section() {
                   alt={`${c.heading} certificate`}
                 />
                 <div
-                  className={`${isDark ? "h-60" : "h-80"} flex flex-col gap-6`}
+                  className={`${isDark ? "max-h-80" : "h-80"} flex flex-col gap-6`}
                 >
                   <div
                     className={`${isDark ? "h-20" : "flex h-30 flex-col justify-between gap-4"} `}
@@ -54,18 +54,19 @@ function Certifications_section() {
                     </span>
                   </div>
                   <p
-                    className={`${isDark ? "text-(--text-primary)" : "mx-2 rounded-md text-(--text-dark) shadow-[2px_2px_5px_#BABECC,-5px_-5px_10px_#ffffff73]"} p-2 text-[12px] leading-relaxed font-bold`}
+                    className={`${isDark ? "text-(--text-primary)" : "mx-2 rounded-md text-(--text-dark) shadow-[2px_2px_5px_#BABECC,-5px_-5px_10px_#ffffff73]"} h-fit p-2 text-[12px] leading-relaxed font-bold`}
                   >
                     {c.content}
                   </p>
                 </div>
+
                 <a
                   href={c.img_url}
                   target="_blank"
                   className="my-5 flex w-full justify-center"
                 >
                   <button
-                    className={`${isDark ? "bg-blue-600 text-white" : "text-black shadow-[2px_2px_5px_#BABECC,-5px_-5px_10px_#ffffff73] hover:shadow-[inset_2px_2px_5px_#BABECC,inset_-5px_-5px_10px_#ffffff73]"} w-[70%] cursor-pointer rounded-2xl p-2 hover:font-extrabold`}
+                    className={`${isDark ? "bg-blue-600 text-white" : "text-black shadow-[2px_2px_5px_#BABECC,-5px_-5px_10px_#ffffff73] hover:shadow-[inset_2px_2px_5px_#BABECC,inset_-5px_-5px_10px_#ffffff73]"} w-[70%] cursor-pointer rounded-2xl p-2 hover:font-extrabold max-md:p-1.5`}
                   >
                     view certificate
                   </button>
